42265d24ed824254c64bf7e66c68e3ed6f2d9ce7
inzord <109310966+inzord@users.noreply.github.com>
2024-08-18 19:39:09 +0300

    upgrade

--- yb_to_mp3.py
+++ yb_to_mp3.py
@@ -1,5 +1,4 @@
 import re
-import threading
 import os
 import logging
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -15,99 +14,60 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 class UrlToMp3:
     def __init__(self, urls: [str], path_out: str, max_threads: int):
-        self.check = None
         self.urls = urls
         self.path_out = path_out
-        self.mp3 = Params().MP3
-        self.mp4 = Params().MP4
-        self.video_regex = Params().VIDEO_REGEX
-        self.youtube_stream_audio = Params().YOUTUBE_STREAM_AUDIO
-        self.playlist_check = Params().PLAYLIST_CHECK
-        self.mix_check = Params().MIX_CHECK
+        self.params = Params()
         self.max_threads = max_threads
+        self.check = None
 
-    def video_to_mp3(self, links: str):
-        logging.info(f"Начало конвертации видео {links} в MP3")
-        yt = YouTube(links)
-        video = yt.streams.filter(only_audio=True).first()
-        out_file = video.download(self.path_out)
+    def download_and_convert_video(self, link: str, only_audio: bool = True):
+        yt = YouTube(link)
+        stream = yt.streams.filter(only_audio=only_audio).first() if only_audio else yt.streams.first()
+        out_file = stream.download(self.path_out)
         base, ext = os.path.splitext(out_file)
-        new_file = base + self.mp3
-        os.rename(out_file, new_file)
-        logging.info(f"Завершение конвертации видео {links} в MP3")
-
-    def mix_playlist_to_mp3(self, links: str):
-        logging.info(f"Начало конвертации микса {links} в MP3")
-        yt = YouTube(links)
-        video = yt.streams.first()
-        out_file = video.download(self.path_out)
-        new_file = out_file.replace(self.mp4, self.mp3)
-        video_clip = AudioFileClip(out_file)
-        video_clip.write_audiofile(new_file)
-        logging.info(f"Завершение конвертации микса {links} в MP3")
-
-    def playlist_to_mp3(self, links: str):
-        logging.info(f"Начало конвертации плейлиста {links} в MP3")
-        playlist = Playlist(links)
-        playlist._video_regex = re.compile(self.video_regex)
-        for video in playlist.videos:
-            out_file = video.streams.get_by_itag(self.youtube_stream_audio).download(self.path_out)
-            new_file = out_file.replace(self.mp4, self.mp3)
+        new_file = base + self.params.MP3
+        if not only_audio:
             video_clip = AudioFileClip(out_file)
             video_clip.write_audiofile(new_file)
-        logging.info(f"Завершение конвертации плейлиста {links} в MP3")
+            video_clip.close()
+        else:
+            os.rename(out_file, new_file)
+        logging.info(f"Конвертация {link} в MP3 завершена")
 
     def process_video(self, link: str):
-        logging.info(f"Начало обработки {link} в потоке {threading.current_thread().name}")
+        logging.info(f"Начало обработки {link}")
         try:
-            if link.find(self.playlist_check) >= 0:
-                if self.check == self.mix_check:
-                    new_data = []
+            if link.find(self.params.PLAYLIST_CHECK) >= 0:
+                if self.check == self.params.MIX_CHECK:
                     soup = get_browser(link)
-                    new_data.append(web_scraping(soup))
-                    logging.info(f"Скрапинг данных для {link} завершен")
-                    for link in new_data:
-                        for lin in link:
-                            self.mix_playlist_to_mp3(lin)
-                else:
-                    self.playlist_to_mp3(link)
-            else:
-                self.video_to_mp3(link)
-        except Exception as ex:
-            logging.error(f"Ошибка при обработке {link}: {ex}")
-        finally:
-            logging.info(f"Завершение обработки {link} в потоке {threading.current_thread().name}")
-
-    def process(self):
-        all_links = []
-        for link in self.urls:
-            if link.find(self.playlist_check) >= 0:
-                self.check = input(f'{self.playlist_check}? or {self.mix_check}?: ')
-                break
-
-        for link in self.urls:
-            if link.find(self.playlist_check) >= 0:
-                if self.check == self.mix_check:
-                    new_data = []
-                    soup = get_browser(link)
-                    new_data.append(web_scraping(soup))
-                    logging.info(f"Скрапинг данных для {link} завершен")
-                    for sub_link in new_data:
-                        all_links.extend(sub_link)
+                    new_data = web_scraping(soup)
+                    for lin in new_data:
+                        self.download_and_convert_video(lin, only_audio=False)
                 else:
                     playlist = Playlist(link)
-                    playlist._video_regex = re.compile(self.video_regex)
-                    all_links.extend([video.watch_url for video in playlist.videos])
+                    playlist._video_regex = re.compile(self.params.VIDEO_REGEX)
+                    for video in playlist.videos:
+                        self.download_and_convert_video(video.watch_url)
             else:
-                all_links.append(link)
+                self.download_and_convert_video(link)
+        except Exception as exp:
+            logging.error(f"Ошибка при обработке {link}: {exp}")
+        finally:
+            logging.info(f"Завершение обработки {link}")
+
+    def process(self):
+        for link in self.urls:
+            if link.find(self.params.PLAYLIST_CHECK) >= 0:
+                self.check = input(f'{self.params.PLAYLIST_CHECK}? or {self.params.MIX_CHECK}?: ')
+                break
 
         with ThreadPoolExecutor(max_workers=self.max_threads) as executor:
-            futures = [executor.submit(self.process_video, link) for link in all_links]
+            futures = [executor.submit(self.process_video, link) for link in self.urls]
             for future in as_completed(futures):
                 try:
                     future.result()
-                except Exception as ex:
-                    logging.error(f"Ошибка при обработке ссылки: {ex}")
+                except Exception as exp:
+                    logging.error(f"Ошибка при обработке ссылки: {exp}")
 
         clean_folder(self.path_out)
 
